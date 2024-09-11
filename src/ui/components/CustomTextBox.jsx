c9979d8122cd7f485eff06bb9d0c19e4d9f5d437
ArjunasDi <134566274+ArjunasDi@users.noreply.github.com>
2024-09-10 21:42:51 -0600

    Update CustomTextBox.jsx
    Cambio Para Deshabilitar El Botón Enviar

--- src/ui/components/CustomTextBox.jsx
+++ src/ui/components/CustomTextBox.jsx
@@ -1,35 +1,47 @@
-
 import { Box, Input, Text } from "@chakra-ui/react";
 import PropTypes from 'prop-types';
 
-function CustomTextBox(props) {
+function CustomTextBox({ title, example, required, name, value, onChange }) {
   return (
     <>
       <Box display="flex" alignItems="center">
         <Text fontSize="xl" marginLeft={3}>
-          {props.title ? props.title : ""}
+          {title || ""}
         </Text>
         <Text color={"red"} fontSize="2xl">
-          {props.required ? "*" : ""}
+          {required ? "*" : ""}
         </Text>
       </Box>
-      <Input placeholder={props.example} variant="filled" marginTop={2} />
+      <Input
+        placeholder={example}
+        variant="filled"
+        marginTop={2}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
     </>
   );
 }
+
 // Default values for the props
 CustomTextBox.defaultProps = {
-    title: "",
-    example: "",
-    required: false,
+  title: "",
+  example: "",
+  required: false,
+  name: "",
+  value: "",
+  onChange: () => {},
 };
 
 // Typechecking for the props
 CustomTextBox.propTypes = {
-    title: PropTypes.string,
-    example: PropTypes.string,
-    required: PropTypes.bool,
+  title: PropTypes.string,
+  example: PropTypes.string,
+  required: PropTypes.bool,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
-
 
 export default CustomTextBox;
