e4a6836e37da74e33f8d9ccf851fa7f09137099b
Kirill Makienko <elchiqvo@gmail.com>
2024-10-15 10:30:39 -0600

    agregar logo

--- src/ui/pages/MainPage.jsx
+++ src/ui/pages/MainPage.jsx
@@ -1,7 +1,8 @@
 import { Grid, Text } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import QuestionForm from "../components/QuestionForm";
-// import Logo from "../../assets/hnm_muestra.png";
+import Logo from "../../assets/hnm_muestra.png";
+import { Image, GridItem } from "@chakra-ui/react";
 
 function MainPage() {
   // eslint-disable-next-line no-unused-vars
@@ -23,18 +24,27 @@ function MainPage() {
     <>
       <Grid
         container
-        alignItems="center" // This aligns the items (text and image) on the same level
+        alignItems="center" // Align items vertically centered
+        justifyContent="center" // Align items horizontally centered
         style={{ minHeight: "5vh" }}
         templateRows="repeat(1, 1fr)"
       >
-        {/* <GridItem w="100%" h="0" color={"black"} colSpan={{ base: 1, md: 1 }} marginStart={20}>
+        <GridItem
+          w="100%"
+          h="0"
+          color="black"
+          colSpan={{ base: 1, md: 1 }}
+          display="flex"
+          justifyContent="center" // Center the image horizontally
+        >
           <Image src={Logo} h={50} w={70} />
-        </GridItem> */}
+        </GridItem>
 
-
-        <Text color="black" style={{ textAlign: "center" }} fontSize="4xl">
+        <GridItem marginTop={50}>
+        <Text color="black" textAlign="center" fontSize="4xl" >
           Sistema de levantamiento de tickets
         </Text>
+        </GridItem>
       </Grid>
 
       <QuestionForm />
